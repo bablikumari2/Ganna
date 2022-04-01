721317bcaaef642a1ac8d87f692cd0e38f99b0f7
bablikumari2 <anandbabli96@gmail.com>
2022-04-01 16:01:53 +0530

    fortend

--- backend/passport.js
+++ backend/passport.js
@@ -40,7 +40,7 @@ passport.use(
     {
       clientID:"3133884073514905",
       clientSecret:"7d5a71e0ebe51b15555ec432e6ee42fc",
-      callbackURL: "/auth/facebook/callback",
+      callbackURL: "https://ganna-app.herokuapp.com/auth/facebook/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,50 @@
+// import Google from "../img/google.png";
+// import Facebook from "../img/facebook.png";
+// import Github from "../img/github.png";
+
+const Login = () => {
+  const google = () => {
+    window.open("https://ganna-app.herokuapp.com/", "_self");
+  };
+
+  const github = () => {
+    window.open("https://ganna-app.herokuapp.com/", "_self");
+  };
+
+  const facebook = () => {
+    window.open("https://ganna-app.herokuapp.com/", "_self");
+  };
+
+  return (
+    <div className="login">
+      <h1 className="loginTitle">Choose a Login Method</h1>
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton google" onClick={google}>
+            <img src={"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpng.pngtree.com%2Felement_our%2Fpng%2F20181117%2Ffacebook-logo-icon-png_241252.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Fso%2Ffacebook&tbnid=ZiZI20cVkC_4xM&vet=12ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ..i&docid=s171Vynh5PaKHM&w=360&h=360&q=facebook%20image%20download&ved=2ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ"} alt="" className="icon" />
+            Google
+          </div>
+          <div className="loginButton facebook" onClick={facebook}>
+            <img src={"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpng.pngtree.com%2Felement_our%2Fpng%2F20181117%2Ffacebook-logo-icon-png_241252.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Fso%2Ffacebook&tbnid=ZiZI20cVkC_4xM&vet=12ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ..i&docid=s171Vynh5PaKHM&w=360&h=360&q=facebook%20image%20download&ved=2ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ"} alt="" className="icon" />
+            Facebook
+          </div>
+          <div className="loginButton github" onClick={github}>
+            <img src={"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpng.pngtree.com%2Felement_our%2Fpng%2F20181117%2Ffacebook-logo-icon-png_241252.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Fso%2Ffacebook&tbnid=ZiZI20cVkC_4xM&vet=12ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ..i&docid=s171Vynh5PaKHM&w=360&h=360&q=facebook%20image%20download&ved=2ahUKEwiNk-_ywvD2AhWXYmwGHQBZC8IQMygHegUIARDoAQ"} alt="" className="icon" />
+            Github
+          </div>
+        </div>
+        <div className="center">
+          <div className="line" />
+          <div className="or">OR</div>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button className="submit">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
